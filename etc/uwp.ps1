8838d3e672142aae11f0477bff0e1ed72248e970
Paul Rouget <me@paulrouget.com>
2020-03-20 10:06:56 +0100

    i1

--- etc/uwp.ps1
+++ etc/uwp.ps1
@@ -19,7 +19,7 @@ Write-Output "PAUL: READING PKG SIGNATURE -----"
 Get-AuthenticodeSignature -FilePath ServoApp_1.0.0.0_x64_Debug.msixbundle | Select-Object *
 
 Write-Output "PAUL: ADD APPX x2 -----"
-# Add-AppxPackage -Path Dependencies\x64\Microsoft.VCLibs.x64.Debug.14.00.appx
+Add-AppxPackage -Path Dependencies\x64\Microsoft.VCLibs.x64.Debug.14.00.appx
 Add-AppxPackage -Path ServoApp_1.0.0.0_x64_Debug.msixbundle
 
 $fam = Get-AppxPackage MozillaFoundation.FirefoxReality | select -expandproperty PackageFamilyName
